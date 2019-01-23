b9ddae7bb88d28ac32b84c415690a104d3224936
Morgan Pierson <morganpierson@gmail.com>
2019-01-22 19:11:54 -0800

    first attempt at networking code

--- GetFlix/MovieDBAPIClient.swift
+++ GetFlix/MovieDBAPIClient.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+
+class MovieDBAPIClient {
+    //for sake of small scope app, simply save api key to fileprivate constant
+    fileprivate let API_KEY = "f6840ab8b085a8f365cf977cf91c3296"
+    
+    //create lazy url to be used later on in app
+    lazy var baseURL: URL = {
+        return URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=\(self.API_KEY)&language=en-US&page=1")!
+    }()
+    
+    //create constant that will handle JSON decoding functionality
+    let decoder = JSONDecoder()
+    
+    //create session constant to be used in api session interaction
+    let session: URLSession
+    
+    //create URLSession instance upon class initialization
+    init(configuration: URLSessionConfiguration) {
+        self.session = URLSession(configuration: configuration)
+    }
+    
+    convenience init() {
+        self.init(configuration: .default)
+    }
+    
+    
+    //create type alias for shorthand closure type definition in getMovies function
+    typealias MovieCompletionHandler = (Movie?, Error?) -> Void
+    
+    //create privat method to handle fetching movie data. func is asynchronous and will function in background
+    private func getMovies(completionHandler completion: @escaping MovieCompletionHandler) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- GetFlix/PopularMovieViewModel.swift
+++ GetFlix/PopularMovieViewModel.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import UIKit
+
+struct PopularMoviesViewModel {
+    let title: String
+    let overview: String
+    let posterImage: UIImage
+    let averageRating: Double
+    
+    init(model: PopularMovies, index: Int) {
+        let movieTitle = model.movies[index].title
+        self.title = movieTitle
+        
+        let movieOverview = model.movies[index].overview
+        self.overview = movieOverview
+        
+        let moviePosterImage = model.movies[index].moviePosterImage
+        self.posterImage = moviePosterImage
+        
+        let averageMovieRating = model.movies[index].averageRating
+        self.averageRating = averageMovieRating
+    }
+}
