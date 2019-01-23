09a9fc82ef9d38add2358dc9cdd0a7a4997da99e
Morgan Pierson <morganpierson@gmail.com>
2019-01-22 16:57:43 -0800

    Initial Commit

---
+++ GetFlix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetFlix
+//
+//  Created by Morgan Pierson on 1/22/19.
+//  Copyright © 2019 Morgan Pierson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
